4fa590ec367e328b54445c36e83dfef13a3a7eb6
Sodiqjon Sodiqov <sodiqjon@example.com>
2025-11-11 20:17:31 +0500

    jkaf

--- main.js
+++ main.js
@@ -1,24 +1,15 @@
 const BOT_TOKEN = '8328125073:AAEWoSW-yjqgPLq4uLPEKGyemwa2lr47x6I';
-const CHAT_ID   = '-4935605017';
+const CHAT_ID = '-4935605017';
 const TELEGRAM_CHANNEL = 'https://t.me/megaaksiya2026';
 
-// Elementlar
 const openModal = document.getElementById('openModal');
 const formModal = document.getElementById('makon-modal');
 const closeModal = document.getElementById('closeModal');
 const makonForm = document.getElementById('makonForm');
 
-// Modalni ochish
-openModal.addEventListener('click', () => {
-  formModal.classList.add('show');  // 'show' klassini qo'shish
-});
+openModal.addEventListener('click', () => formModal.classList.add('show'));
+closeModal.addEventListener('click', () => formModal.classList.remove('show'));
 
-// Modalni yopish
-closeModal.addEventListener('click', () => {
-  formModal.classList.remove('show'); // 'show' klassini olib tashlash
-});
-
-// Formani yuborish
 makonForm.addEventListener('submit', async (e) => {
   e.preventDefault();
 
@@ -33,10 +24,9 @@ makonForm.addEventListener('submit', async (e) => {
   const message = `📝 Yangi so‘rov:\n👤 Ism: ${name}\n📞 Telefon: ${phone}\n🌐 Sayt: 2 oq sayt`;
 
   try {
-    // Telegram botga xabar yuborish
     await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
       method: 'POST',
-      headers: {'Content-Type': 'application/json'},
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         chat_id: CHAT_ID,
         text: message,
@@ -44,11 +34,8 @@ makonForm.addEventListener('submit', async (e) => {
       })
     });
 
-    // Formani yopish va reset qilish
     formModal.classList.remove('show');
     makonForm.reset();
-
-    // Darhol Telegram kanalga yo'naltirish
     window.location.href = TELEGRAM_CHANNEL;
 
   } catch (error) {
